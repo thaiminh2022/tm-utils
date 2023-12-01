9903d3c2b89d9c4f38e9863e8481ec4656c508d7
= <=>
2023-12-02 06:03:45 +0700

    Init + Add mantine

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import '@mantine/core/styles.css';
+
+import { MantineProvider } from '@mantine/core';
+
+function App() {
+    return <MantineProvider>{/* Your app here */}</MantineProvider>;
+}
+export default App
